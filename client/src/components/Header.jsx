bbdee00144a2628fc5c91e987c8f28cfa352e4b1
Sobirov3636 <sobirov3636@gmail.com>
2024-01-24 23:57:26 +0900

    create sign up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -23,7 +23,7 @@ export default function Header() {
         <Button className='w-12 h-10 hidden sm:inline ' color='gray ' pill>
           <FaMoon />
         </Button>
-        <Link to='/sing-in'>
+        <Link to='/sign-in'>
           <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
           </Button>
